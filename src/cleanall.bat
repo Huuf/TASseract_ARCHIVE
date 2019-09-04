e77f73afb43868513dc96b87d245bea9de7b54ac
Justin S <Ownasaurus@gmail.com>
2019-09-03 21:59:09 -0400

    removed hardcoded paths in favor of setenv.bat

--- src/cleanall.bat
+++ src/cleanall.bat
@@ -1,2 +1,2 @@
-"C:\Program Files (x86)\GnuWin32\bin\make.exe" -f makefile_m4.mk clean
-"C:\Program Files (x86)\GnuWin32\bin\make.exe" -f makefile_m7.mk clean
+make -f makefile_m4.mk clean
+make -f makefile_m7.mk clean
--- src/makeall.bat
+++ src/makeall.bat
@@ -1,2 +1,2 @@
-"C:\Program Files (x86)\GnuWin32\bin\make.exe" -f makefile_m4.mk
-"C:\Program Files (x86)\GnuWin32\bin\make.exe" -f makefile_m7.mk
+make -f makefile_m4.mk
+make -f makefile_m7.mk
